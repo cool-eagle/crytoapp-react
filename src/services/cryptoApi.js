b60b05707cfb3294b9b064557c643c50965673d9
yongri910 <yongri910@gmail.com>
2022-01-19 23:29:21 +0800

    window resize responsive

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -26,24 +26,6 @@ export const cryptoApi=createApi({
     })
 })
 
-// const createRequest=(url)=>({url, headers: cryptoApiHeaders})
-
-// export const cryptoApi=createApi({
-//     reducerPath: 'cryptoApi',
-//     baseQuery: fetchBaseQuery({baseUrl}),
-//     endpoints: (builder)=>({
-//         getCryptos: builder.query({
-//             query: (count)=> createRequest(`/coins?limit=${count}`),
-//         }),
-//         getCryptoDetails: builder.query({
-//             query: (coinId)=> createRequest(`/coin/${coinId}`),
-//         }),
-//         getCryptoHistory: builder.query({
-//             query: ({coinId, timePeriod})=> ({url: `/coin/${coinId}/history`,params: {timePeriod: `${timePeriod}`},headers: cryptoApiHeaders}),
-//         })
-//     })
-// })
-
 export const {
     useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
 }=cryptoApi;
